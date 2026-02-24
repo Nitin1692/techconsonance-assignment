32f1ca0fc3ca5b521d9173e23663d71d73d6d1ca
Nitin <nitinjain7201@gmail.com>
2026-02-24 22:35:06 +0530

    chore: response wrapped in response entity

--- src/main/java/com/techconsonance/notes/controllers/DeviceNoteController.java
+++ src/main/java/com/techconsonance/notes/controllers/DeviceNoteController.java
@@ -4,6 +4,7 @@ import com.techconsonance.notes.dto.*;
 import com.techconsonance.notes.services.DeviceNoteService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,21 +18,29 @@ public class DeviceNoteController {
     private final DeviceNoteService service;
 
     @PostMapping
-    public DeviceNoteResponse createNote(
+    public ResponseEntity<DeviceNoteResponse> createNote(
             @PathVariable Long deviceId,
-            @RequestHeader(value="X-User", required=false) String user,
+            @RequestHeader(value = "X-User", required = false) String user,
             @RequestBody CreateNoteRequest req) {
 
         log.info("Create note request deviceId={} user={}", deviceId, user);
-        return service.createNote(deviceId, user, req);
+
+        DeviceNoteResponse response = service.createNote(deviceId, user, req);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)   // 201
+                .body(response);
     }
 
     @GetMapping
-    public List<DeviceNoteResponse> listNotes(
+    public ResponseEntity<List<DeviceNoteResponse>> listNotes(
             @PathVariable Long deviceId,
-            @RequestParam(required=false) Integer limit) {
+            @RequestParam(required = false) Integer limit) {
 
         log.info("List notes request deviceId={}", deviceId);
-        return service.listNotes(deviceId, limit);
+
+        List<DeviceNoteResponse> response = service.listNotes(deviceId, limit);
+
+        return ResponseEntity.ok(response);  // 200
     }
 }
